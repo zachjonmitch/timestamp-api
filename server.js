a39846b299edf45fa1029805930189a97b704111
Zach Mitchell <zjmitche@gmail.com>
2017-02-26 07:01:17 +0000

    moved file

---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 8080;
+var path = require('path');
+
+
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname + '/../client/index.html'));
+});
+app.get('/:timestamp', function(request, response) {
+    var timestamp = request.params.timestamp;
+    response.json(getTimestampJSON(timestamp));
+});
+
+app.listen(PORT, function() {
+    console.log('Listening on port ' + PORT);
+});
+
+function getTimestampJSON(timestamp) {
+    var result = {
+        unix: null,
+        natural: null
+    };
+    
+    var date;
+    if(!isNaN(parseInt(timestamp))) {
+        date = new Date(parseInt(timestamp));
+    } else {
+        date = new Date(timestamp);
+    }
+    
+    if (!isNaN(date.getTime())) {
+        result.unix = date.getTime();
+        result.natural = getNaturalDate(date);
+    }
+    
+    return result;
+}
+
+function getNaturalDate(date) {
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    return months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
+}
